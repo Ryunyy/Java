e05ca12a58b678c49a7daa69decf9fc72cef9232
Ryunyy <khirigiri@gmail.com>
2022-08-17 20:00:01 +0700

    fix grab for ram

--- src/main/java/Disk.java
+++ src/main/java/Disk.java
@@ -39,13 +39,14 @@ public class Disk extends Element{
         String temp = "";
         String[] parts = parse_res.get(0).split(" ");
         for(int i = 0;i < parts.length; i++) {
-            if(!parts[i].contains(" ")) {
+            if(parts[i].length() > 1) {
                 temp1 = Double.valueOf(parts[i]) / 1024 / 1024;
                 temp2 = Double.valueOf(parts[i+1]) / 1024 / 1024;
                 temp = String.format("%.2f", temp1);
                 this.total_rom = Double.valueOf(temp);
                 temp = String.format("%.2f", temp2);
                 this.used_rom = Double.valueOf(temp);
+                i = parts.length;
             }
         }
 
